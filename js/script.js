d826c8c1b6d660052ed6fb2d0c5ec3b3e2fd41e0
mohsom <volodya.skalskyi@gmail.com>
2015-04-26 18:20:19 +0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by mohsom on 25.04.2015.
  */
+$("document").ready(function () {
+    $(".menu-item-humb").click(function () {
+        $(".menu-items").toggleClass("visible");
+    });
+});
